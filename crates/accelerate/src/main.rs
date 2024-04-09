cd4f4400255c74bf897a5cbd7b3c401ac563aca5
Arnau Casau Playa <arnau.casau.playa@ibm.com>
2024-04-09 16:34:24 +0200

    add main.rs to test the performance

---
+++ crates/accelerate/src/main.rs
@@ -0,0 +1,66 @@
+use ndarray::Array;
+use std::time::Instant;
+use faer::{Mat};
+//use faer::{Mat, Parallelism};
+//use faer::modules::core::mul::matmul;
+use num_complex::Complex64;
+
+use rand::distributions::{Distribution, Uniform};
+
+use std::env;
+
+//const ALPHA: Complex64 = Complex64::new(0.5,3.);
+//const BETA: Complex64 = Complex64::new(1.,1.);
+const N: u32 = 1000;
+
+fn rand_num()-> f64{
+    let step = Uniform::new(-1., 1.);
+    let mut rng = rand::thread_rng();
+    let choice = step.sample(&mut rng);
+    return choice;
+}
+
+fn ndarray_mat_mul(size: usize){
+    let a: Array<Complex64,_> = Array::from_elem((size, size), Complex64::new(rand_num(), rand_num()));
+    let mut b: Array<Complex64,_> = Array::from_elem((size, size), Complex64::new(rand_num(), rand_num()));
+    
+    let start = Instant::now();
+    for _ in 0..N {
+        b = a.dot(&b);
+    }
+    println!("Elapsed time ndarray: {:.2?}", start.elapsed()/N);
+}
+
+fn faer_mat_mul(size: usize){
+    let a = Mat::<Complex64>::from_fn(size, size, |_, _| Complex64::new(rand_num(), rand_num()));
+    let mut b = Mat::<Complex64>::from_fn(size, size, |_, _| Complex64::new(rand_num(), rand_num()));
+    
+    let start = Instant::now();
+    for _ in 0..N {
+        //matmul(b.as_mut(), a.as_ref(), a.as_ref(), Some(ALPHA), BETA, Parallelism::None);
+        b = &a * &b;
+    }
+    println!("Elapsed time faer: {:.2?}", start.elapsed()/N);
+}
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    let arg = match args.get(1) {
+        Some(val) => val,
+        None => {
+            println!("Not enough arguments provided!");
+            return;
+        }
+    };
+
+    let size = match arg.parse::<usize>() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("Unable to parse number from argument: {}", e);
+            return;
+        }
+    };
+
+    ndarray_mat_mul(size);
+    faer_mat_mul(size);
+}
