9b0c71b6858632b33f5ee065a5b9b9e494028510
Arnau Casau <arnaucasau@gmail.com>
2024-04-10 16:48:52 +0200

    results pc and remove comments

--- crates/accelerate/src/main.rs
+++ crates/accelerate/src/main.rs
@@ -4,14 +4,16 @@ use std::time::Instant;
 use faer::{Mat, Parallelism};
 use faer::modules::core::mul::matmul;
 use num_complex::Complex64;
-
 use rand::distributions::{Distribution, Uniform};
 
 use std::env;
 
-const ALPHA: Complex64 = Complex64::new(1.,1.);
-const BETA: Complex64 = Complex64::new(0.5,3.);
-const N: u32 = 1000;
+const ALPHA: Complex64 = Complex64::new(1.,0.);
+const BETA: Complex64 = Complex64::new(0.5,0.5);
+
+const ITERATIONS: u32 = 1000;
+const N: usize = 8; // size of the matrices
+
 
 fn rand_num()-> f64{
     let step = Uniform::new(-1., 1.);
@@ -20,47 +22,37 @@ fn rand_num()-> f64{
     return choice;
 }
 
-fn ndarray_mat_mul(size: usize){
-    // ndarray using Array
-    let a: Array<Complex64,_> = Array::from_elem((size, size), Complex64::new(rand_num(), rand_num()));
-    let b: Array<Complex64,_> = Array::from_elem((size, size), Complex64::new(rand_num(), rand_num()));
-
-    // ndarray using aview2 fixed to 4x4
-    let data_a_aview: [[Complex64; 4]; 4] = [[Complex64::new(rand_num(), rand_num());4];4];
+fn ndarray_mat_mul(){
+    let data_a_aview: [[Complex64; N]; N] = [[Complex64::new(rand_num(), rand_num());N];N];
     let a_aview = aview2(&data_a_aview);
-    let data_b_aview: [[Complex64; 4]; 4] = [[Complex64::new(rand_num(), rand_num());4];4];
+    let data_b_aview: [[Complex64; N]; N] = [[Complex64::new(rand_num(), rand_num());N];N];
     let b_aview = aview2(&data_b_aview);
 
-    let mut dst: Array<Complex64,_> = Array::zeros([size, size]);
+    let mut dst: Array<Complex64,_> = Array::zeros([N, N]);
 
     let start = Instant::now();
-    for _ in 0..N {
-        // Equivalence of the faer's matmul using Array
-        //dst = ALPHA * dst + BETA * a.dot(&b);
-
-        // Equivalence of the faer's matmul using Array
+    for _ in 0..ITERATIONS {
+        // Equivalence of the faer's matmul using dot
         dst = ALPHA * dst + BETA * a_aview.dot(&b_aview);
-
-        // Simple multiplication
-        //dst = a.dot(&b);
     }
-    println!("Elapsed time ndarray: {:.2?}", start.elapsed()/N);
+
+    //println!("Elapsed time ndarray: {:.2?}", start.elapsed()/ITERATIONS);
+    println!("{:.2?}", start.elapsed()/ITERATIONS);
 }
 
-fn faer_mat_mul(size: usize){
-    let a = Mat::<Complex64>::from_fn(size, size, |_, _| Complex64::new(rand_num(), rand_num()));
-    let b = Mat::<Complex64>::from_fn(size, size, |_, _| Complex64::new(rand_num(), rand_num()));
-    let mut dst = Mat::<Complex64>::zeros(size, size);
+fn faer_mat_mul(){
+    let a = Mat::<Complex64>::from_fn(N, N, |_, _| Complex64::new(rand_num(), rand_num()));
+    let b = Mat::<Complex64>::from_fn(N, N, |_, _| Complex64::new(rand_num(), rand_num()));
+    let mut dst = Mat::<Complex64>::zeros(N, N);
     
     let start = Instant::now();
-    for _ in 0..N {
+    for _ in 0..ITERATIONS {
         // matmul -> dst = ALPHA * dst + BETA * &a * &b;
         matmul(dst.as_mut(), a.as_ref(), b.as_ref(), Some(ALPHA), BETA, Parallelism::None);
-
-        // Simple multiplication
-        //dst = &a * &b;
     }
-    println!("Elapsed time faer: {:.2?}", start.elapsed()/N);
+
+    //println!("Elapsed time faer: {:.2?}", start.elapsed()/ITERATIONS);
+    println!("{:.2?}", start.elapsed()/ITERATIONS);
 }
 
 fn main(){
@@ -73,7 +65,7 @@ fn main(){
         }
     };
 
-    let size = match arg.parse::<usize>() {
+    let method = match arg.parse::<i32>() {
         Ok(val) => val,
         Err(e) => {
             println!("Unable to parse number from argument: {}", e);
@@ -81,6 +73,11 @@ fn main(){
         }
     };
 
-    ndarray_mat_mul(size);
-    faer_mat_mul(size);
+    if method == 1 || method == 3 {
+        ndarray_mat_mul();
+    }
+    
+    if method == 2 || method == 3 {
+        faer_mat_mul();
+    }
 }
